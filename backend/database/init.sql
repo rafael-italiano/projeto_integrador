4b1cf850b228fe141f6e06705e3f71fbc5c2fdd0
Rafael Italiano <rafael.italiano@digibee.com>
2024-04-06 12:48:40 -0300

    RFixed init.sql during database build

--- backend/database/init.sql
+++ backend/database/init.sql
@@ -4,15 +4,5 @@ CREATE TABLE event (
     end_timestamp VARCHAR NOT NULL,
     all_day BOOLEAN NOT NULL,
     url VARCHAR[100] NOT NULL,
-    end_timestamp VARCHAR NOT NULL UNIQUE
+    description VARCHAR NOT NULL UNIQUE
 );
-
-title: str
-    start_timestamp: int
-    end_timestamp: int
-    all_day: bool
-    url: str
-    description: str
-
-
-    www.
